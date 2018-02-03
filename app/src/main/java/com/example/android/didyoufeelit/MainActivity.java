60ae3bb2037982a4076bf1384f6fc47dd0aea771
John Hancock <jhancock1975@gmail.com>
2018-02-03 08:35:54 -0500

    download url in background

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,9 +15,12 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import java.net.URL;
 
 /**
  * Displays the perceived strength of a single earthquake event based on responses from people who
@@ -29,16 +32,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-05-02&minfelt=50&minmagnitude=5";
 
+    private Event earthquake;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
+        new MainActivity.DownloadFilesTask().execute(USGS_REQUEST_URL);
 
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+
     }
 
     /**
@@ -53,5 +55,21 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    private class DownloadFilesTask extends AsyncTask<String, Void, Event> {
+        protected Event doInBackground(String... urls) {
+
+
+            earthquake = Utils.fetchEarthquakeData(urls[0]);
+
+            return earthquake;
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            // Update the information displayed to the user.
+            updateUi(earthquake);
+        }
     }
 }
